c00f29a932bf6b0f771f23c3ac0a30d81dd94f88
김민수 MINSOO KIM <alstn113@gmail.com>
2023-05-18 16:10:59 +0900

    feat: createStore, useStore base

--- examples/react-ts/src/stores/counterStore.ts
+++
@@ -1,7 +0,0 @@
-import { createStore } from '../../../../dist';
-
-const counterStore = createStore<{
-  count: number;
-}>({ count: 0 });
-
-export default counterStore;
--- src/lib/store.ts
+++ src/lib/store.ts
@@ -1,25 +1,41 @@
 import { useCallback, useSyncExternalStore } from 'react';
 
-type Listener = () => void;
+type Listener<T> = (state: T, prevState: T) => void;
 
-export const createStore = <T>(initialState: T) => {
+export interface CreateStoreReturnType<T> {
+  setState: (fn: (state: T) => T) => void;
+  getState: () => T;
+  subscribe: (listener: Listener<T>) => void;
+}
+
+export const createStore = <T>(initialState: T): CreateStoreReturnType<T> => {
   let state = initialState;
-  const getState = (): T => state;
-  const listeners = new Set<Listener>();
+  const listeners = new Set<Listener<T>>();
+
+  const getState = () => state;
   const setState = (fn: (state: T) => T) => {
+    const prevState = state;
     state = fn(state);
-    listeners.forEach((l) => l());
+    listeners.forEach((listener) => listener(state, prevState));
   };
-  const subscribe = (listener: Listener) => {
+  const subscribe = (listener: Listener<T>) => {
     listeners.add(listener);
     return () => listeners.delete(listener);
   };
-  return { getState, setState, subscribe };
+
+  return {
+    getState,
+    setState,
+    subscribe,
+  };
 };
 
 export const useStore = <T>(
-  store: { getState: () => T; subscribe: (listener: Listener) => () => void },
-  selector: (state: T) => any,
+  store: {
+    getState: () => T;
+    subscribe: (listener: Listener<T>) => () => void;
+  },
+  selector: (state: T) => T,
 ) => {
   return useSyncExternalStore(
     store.subscribe,
