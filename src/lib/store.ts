336f901d419dd5f29bffc601144546f3556150f5
김민수 MINSOO KIM <alstn113@gmail.com>
2023-05-19 13:46:24 +0900

    feat: draft store

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -1,48 +1,90 @@
 import { useCallback, useSyncExternalStore } from 'react';
+import { isDraftable, produce, type Draft } from 'immer';
 
-type Listener<T> = (state: T, prevState: T) => void;
-type SetState<T> = (state: T) => T;
+type Fn = () => void;
+type UpdateFn<T> = (state: T) => T;
 
-export interface CreateStoreReturnType<T> {
-  setState: (fn: (state: T) => T) => void;
+export type ActionRecord<T> = Record<
+  string,
+  (...params: any[]) => T | Draft<T> | void
+>;
+type Action<T, A> = A extends ActionRecord<T> ? A : never;
+export type Actions<T, A> = (prevState: Draft<T> | T) => Action<T, A>;
+
+const isUpdateFn = <T>(value: any): value is UpdateFn<T> =>
+  typeof value === 'function';
+
+export type StoreInstance<T, A> = {
+  initialState: T;
+  setState: (update: UpdateFn<T> | T) => void;
   getState: () => T;
-  subscribe: (listener: Listener<T>) => void;
-}
+  subscribe: (listener: Fn) => void;
+  actions: Action<T, A> | null;
+};
 
-export const createStore = <T>(initialState: T): CreateStoreReturnType<T> => {
+export const createStore = <T, A>(
+  initialState: T,
+  createActions?: Actions<T, A>,
+): StoreInstance<T, A> => {
   let state = initialState;
-  const listeners = new Set<Listener<T>>();
+  const listeners = new Set<Fn>();
 
   const getState = () => state;
-  const setState = (nextState: T | SetState<T>) => {
-    const prevState = state;
-    state =
-      typeof nextState === 'function'
-        ? (nextState as SetState<T>)(state)
-        : nextState;
-    listeners.forEach((listener) => listener(state, prevState));
+  const setState = (update: UpdateFn<T> | T) => {
+    state = isUpdateFn<T>(update) ? update(state) : update;
+    listeners.forEach((listener) => listener());
   };
-  const subscribe = (listener: Listener<T>) => {
+  const subscribe = (listener: Fn) => {
     listeners.add(listener);
     return () => listeners.delete(listener);
   };
 
+  const actions = (() => {
+    if (!createActions) return null;
+    type ActionKey = keyof Action<T, A>;
+    const record = createActions(initialState);
+    const keys = Object.keys(record) as ActionKey[];
+    keys.forEach((key) => {
+      record[key] = ((...params: any[]) => {
+        setState((prevState) => {
+          if (!isDraftable(prevState)) {
+            const action = createActions(prevState)[key];
+            const next = action(...params);
+            return next as any;
+          }
+          const produced = produce(prevState, (draft) => {
+            const action = createActions(draft)[key];
+            const result = action(...params);
+            if (result !== undefined) {
+              return result as Draft<T>;
+            }
+          });
+          return produced;
+        });
+      }) as Action<T, A>[ActionKey];
+    });
+    return record;
+  })();
+
   return {
+    initialState,
     getState,
     setState,
     subscribe,
+    actions,
   };
 };
 
-export const useStore = <T>(
+export function useStore<T>(store: StoreInstance<T, any>);
+export function useStore<T>(
   store: {
     getState: () => T;
-    subscribe: (listener: Listener<T>) => () => void;
+    subscribe: (listener: Fn) => () => void;
   },
   selector: (state: T) => T,
-) => {
+) {
   return useSyncExternalStore(
     store.subscribe,
     useCallback(() => selector(store.getState()), [store, selector]),
   );
-};
+}
