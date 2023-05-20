ab1bd1d6fa7c96127eed881bfc8d25e0651047b4
김민수 MINSOO KIM <alstn113@gmail.com>
2023-05-20 16:46:27 +0900

    refactor: vanilla store 제거

--- src/lib/create.ts
+++ src/lib/create.ts
@@ -15,22 +15,10 @@ type Create = {
   <T>(): <Mos extends [StoreMutatorIdentifier, unknown][] = []>(
     initializer: StateCreator<T, [], Mos>,
   ) => UseBoundStore<Mutate<StoreApi<T>, Mos>>;
-  /**
-   * @deprecated Use `useStore` hook to bind store
-   */
-  <S extends StoreApi<unknown>>(store: S): UseBoundStore<S>;
 };
 
 const createImpl = <T>(createState: StateCreator<T, [], []>) => {
-  // createState가 function이 아니면 이거는 vanilla store이다.
-  // 그니까 이걸 어떻게 해야하지?
-  if (typeof createState !== 'function') {
-    console.warn(
-      "[DEPRECATED] Passing a vanilla store will be unsupported in a future version. Instead use `import { useStore } from 'zustand'`.",
-    );
-  }
-  const api =
-    typeof createState === 'function' ? createStore(createState) : createState;
+  const api = createStore(createState);
 
   // useStore를 사용할 때마다 api를 전달하지 않아도 되도록 useBoundStore를 만든다.
   // useBoundStore는 useStore와 같은 기능을 하지만 api를 전달하지 않아도 된다.
