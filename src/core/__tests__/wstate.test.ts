70a92547ad7b0c860834b8900fb5fedee67d3a2b
김민수 MINSOO KIM <alstn113@gmail.com>
2023-05-31 00:05:46 +0900

    fix: eslint react version

--- src/core/__tests__/wstate.test.ts
+++ src/core/__tests__/wstate.test.ts
@@ -1,5 +1,5 @@
 import { wstate } from '../wstate';
-import { act, renderHook } from '@testing-library/react-hooks';
+import { renderHook } from '@testing-library/react-hooks';
 
 describe('wstate', () => {
   test('basic', () => {
